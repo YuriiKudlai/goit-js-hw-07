46efb9cc184a224e0a1fa0dd473ed04554282c07
YuriiKudlai <kudlai.yurii@gmail.com>
2022-06-18 14:31:43 +0300

    update all tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+// console.log(galleryContainer);
+
+const galleryMarkup = createCardsGallery(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createCardsGallery(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+          return  `<div class="gallery__item">
+  <a class="gallery__link" href="${original}" onclick = 'event.preventDefault()'>
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+        })
+        .join('');
+}
+
+
+function onGalleryContainerClick(event) {
+    const isGalleryImageEl = event.target.classList.contains('gallery__image');
+
+    if (!isGalleryImageEl) {
+        return;
+    } else {
+        const instance = basicLightbox.create(`
+    <img src= "${event.target.dataset.source}"/>
+`,
+    { onShow: () => {window.addEventListener('keydown', onEscPress) } },
+    );
+
+      instance.show();
+      function onEscPress(event) {
+        if (event.code === 'Escape') {
+            instance.close();
+            window.removeEventListener('keydown', onEscPress);
+        }
+      }
+    }
+    
+    console.log(event.target);
+}
+
+
+
+
+
+
+// console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createCardsGallery(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createCardsGallery(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+          return  `<div class="gallery__item">
+  <a class="gallery__item" href="${original}" onclick = 'event.preventDefault()'>
+  <img class="gallery__image" 
+  src="${preview}" 
+  title="${description}" />
+</a>
+</div>`;
+        })
+        .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "title", captionDelay: 250  });
 
 console.log(galleryItems);
+
+
